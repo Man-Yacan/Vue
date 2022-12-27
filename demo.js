4a687b626bbe4ad771174fd035348cf8eb9915cf
重生少年 <39402098+Man-Yacan@users.noreply.github.com>
2022-12-27 21:52:03 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,10 +1,5 @@
-// 准备html字符串
-let htmlStr = ''
-// 遍历数据拼接html字符串
-persons.forEach(p => {
-    htmlStr += `<li>${p.id} - ${p.name} - ${p.age}</li>`
-});
-// 获取list元素
-let list = document.getElementById('list')
-// 修改内容（亲自操作DOM）
-list.innerHTML = htmlStr
+const persons = [
+    { id: '001', name: '张三', age: '18' },
+    { id: '002', name: '李四', age: '19' },
+    { id: '003', name: '王麻子', age: '20' }
+]
